3417b5e7eb8e219161283187d98581e15ad09df2
Marcell28100 <pozsgaim@kkszki.hu>
2025-02-10 08:03:30 +0100

    Created the API, added a connectionstring and made the base template for the Controller

---
+++ LibraryDolgozat/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryDolgozat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        //GET
+        //GET ID
+        //POST
+        //PUT
+        //DELETE
+    }
+}
---
+++ LibraryDolgozat/Models/LibraryContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryDolgozat.Models;
+
+public partial class LibraryContext : DbContext
+{
+    public LibraryContext()
+    {
+    }
+
+    public LibraryContext(DbContextOptions<LibraryContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("books");
+
+            entity.Property(e => e.Id).HasColumnType("int(11)");
+            entity.Property(e => e.Author).HasMaxLength(255);
+            entity.Property(e => e.Genre)
+                .HasMaxLength(100)
+                .HasDefaultValueSql("'NULL'");
+            entity.Property(e => e.Price)
+                .HasPrecision(10)
+                .HasDefaultValueSql("'NULL'");
+            entity.Property(e => e.PublishedYear).HasColumnType("int(11)");
+            entity.Property(e => e.Title).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
